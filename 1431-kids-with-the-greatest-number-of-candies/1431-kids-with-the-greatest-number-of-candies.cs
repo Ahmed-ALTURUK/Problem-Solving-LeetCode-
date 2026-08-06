9794faafe455d9f08aa1157ea3ed1fd1e186f86e
Ahmed Alturuk <ahmadalturuk551@gmail.com>
2026-08-06 15:21:56 +0300

    Time: 0 ms (100%), Space: 48.8 MB (76.23%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
@@ -1,24 +1,23 @@
 public class Solution {
+
+    //The time complextiy of this function is O(n)
     public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
-    
-        int greatestNum = 0;
-        bool[] result = new bool[candies.Length];
-
-        for(int i = 0; i < candies.Length;i++)
-        {
-            greatestNum = candies[i] + extraCandies;
-
-            for(int j = 0; j < candies.Length;j++)
-            {
-                if(candies[j] > greatestNum)
-                {
-                 result[i] = false;
-                 break;
-                }
-                else
-                 result[i] = true;
+        
+       int maxCandies = 0;
+        for (int i = 0; i < candies.Length; i++) {
+            if (candies[i] > maxCandies) {
+                maxCandies = candies[i];
             }
         }
+
+        bool[] result = new bool[candies.Length];
+
+        for (int i = 0; i < candies.Length; i++) 
+        {
+            result[i] = (candies[i] + extraCandies) >= maxCandies;
+        }
+
+        return result;
 
         return result;
     }
