81137a832aced8630835bb1250e153181173cbae
Ahmed Alturuk <ahmadalturuk551@gmail.com>
2026-08-08 16:16:39 +0300

    Time: 0 ms (100%), Space: 48.7 MB (97.55%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cs
+++ 0724-find-pivot-index/0724-find-pivot-index.cs
@@ -1,38 +1,24 @@
 public class Solution {
     public int PivotIndex(int[] nums) {
-       
-        int[] letfSumArr = new int[nums.Length];
-        int[] RightSumArr = new int[nums.Length];
+        int totalSum = 0;
+        int leftSum = 0;
 
-       for(int i = 0; i < nums.Length; i++)
-        {
-            int leftSum = 0;
-            int rightSum = 0;
-
-          
-            int left = 0;
-            int right = nums.Length - 1;
-
-            while(left < i)
-            {
-                leftSum += nums[left];
-                left++;
-            }
-
-            while(right > i)
-            {
-                rightSum += nums[right];
-                right--;
-            }
-
-            letfSumArr[i] = leftSum;
-            RightSumArr[i] = rightSum;
+     
+        for (int i = 0; i < nums.Length; i++) {
+            totalSum += nums[i];
         }
 
-        for(int j = 0; j < nums.Length; j++)
-        {
-            if(letfSumArr[j] == RightSumArr[j]) return j;
+      
+        for (int i = 0; i < nums.Length; i++) {
+         
+            int rightSum = totalSum - leftSum - nums[i];
 
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+          
+            leftSum += nums[i];
         }
 
         return -1;
